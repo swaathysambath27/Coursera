3094cdbb37a48a4ddc809b276f9243ba1896d595
swaathysambath27 <swaathy.sambath@gmail.com>
2021-08-24 16:54:18 +0200

    first commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
+import React,{ Component } from 'react';
 import logo from './logo.svg';
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponents';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1> Hello from swaathy</h1>
+      <Navbar dar color= "primary">
+        <div className = "container">
+          <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+        </div>
+      </Navbar>
+      <Menu/>
     </div>
   );
 }
